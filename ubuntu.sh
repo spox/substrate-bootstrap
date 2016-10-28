3ee0cb58ed9d559ee911cd2541f3aad8b06bcc33
Chris Roberts <code@chrisroberts.org>
2016-10-28 15:02:52 -0700

    Dump into custom directory

--- ubuntu.sh
+++ ubuntu.sh
@@ -6,4 +6,5 @@ gem install json_pure -v '~> 1.0' --no-ri --no-rdoc
 gem install puppet -v '~> 3.0' --no-ri --no-rdoc
 ln -s /var/lib/gems/1.8/bin/puppet /usr/local/bin/puppet
 
-/vagrant/substrate/run.sh /vagrant
+mkdir -p /vagrant/substrate-assets
+/vagrant/substrate/run.sh /vagrant/substrate-assets
