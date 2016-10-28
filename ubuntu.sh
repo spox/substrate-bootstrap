dffb0093bcd87606afc5105500b4a645fd0b091f
Chris Roberts <code@chrisroberts.org>
2016-10-28 10:51:32 -0700

    run substrate command directly

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,5 +4,6 @@ DEBIAN_FRONTEND=noninteractive apt-get update -yq
 DEBIAN_FRONTEND=noninteractive apt-get install -yq ruby rubygems1.8
 gem install json_pure -v '~> 1.0' --no-ri --no-rdoc
 gem install puppet -v '~> 3.0' --no-ri --no-rdoc
-
 ln -s /var/lib/gems/1.8/bin/puppet /usr/local/bin/puppet
+
+/vagrant/substrate/run.sh /vagrant
