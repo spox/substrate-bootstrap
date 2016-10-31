4cbf56698f90016df56cc3b65af6ecc8c384eeb3
Chris Roberts <code@chrisroberts.org>
2016-10-31 06:19:11 -0700

    allow root install

--- osx.sh
+++ osx.sh
@@ -2,6 +2,6 @@
 
 gem install json_pure -v '~> 1.0' --no-ri --no-rdoc
 gem install puppet -v '~> 3.0' --no-ri --no-rdoc
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install | sed 's/^.+uid.*zero.+$//')"
 mkdir -p /vagrant/substrate-assets
 /vagrant/substrate/run.sh /vagrant/substrate-assets
