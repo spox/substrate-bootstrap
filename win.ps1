57efebd09665ab37e9115f92549ed79f721044a2
Chris Roberts <code@chrisroberts.org>
2016-10-31 16:18:47 -0700

    install puppet directly

--- win.ps1
+++ win.ps1
@@ -9,14 +9,14 @@
 $DotNetUpgradeURL = "https://download.microsoft.com/download/E/2/1/E21644B5-2DF2-47C2-91BD-63C560427900/NDP452-KB2901907-x86-x64-AllOS-ENU.exe"
 $BuildToolsURL = "https://download.microsoft.com/download/E/E/D/EEDF18A8-4AED-4CE0-BEBE-70A83094FC5A/BuildTools_Full.exe"
 $WixURL = "http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=wix&DownloadId=1587179&FileTime=131118854865270000&Build=21031"
-$PuppetInstallURL = "https://raw.githubusercontent.com/hashicorp/puppet-bootstrap/master/windows.ps1"
+$PuppetURL = "https://downloads.puppetlabs.com/windows/puppet-3.8.7.msi"
 
 $TmpDir = [System.IO.Path]::GetTempPath()
 
 $DotNetUpgradeDestination = [System.IO.Path]::Combine($TmpDir, "dotnet-upgrade.exe")
 $BuildToolsDestination = [System.IO.Path]::Combine($TmpDir, "build.exe")
 $WixDestination = [System.IO.Path]::Combine($TmpDir, "wix.exe")
-$PuppetInstallDestination = [System.IO.Path]::Combine($TmpDir, "puppet.ps1")
+$PuppetDestination = [System.IO.Path]::Combine($TmpDir, "puppet.msi")
 
 $WebClient = New-Object System.Net.WebClient
 
@@ -29,9 +29,9 @@ Write-Host "Windows Build Tools successfully downloaded."
 Write-Host "Downloading Wix toolset."
 $WebClient.DownloadFile($WixURL, $WixDestination)
 Write-Host "Wix toolset successfully downloaded."
-Write-Host "Downloading puppet installation powershell script."
-$WebClient.DownloadFile($PuppetInstallURL, $PuppetInstallDestination)
-Write-Host "Puppet installation powershell script successfully downloaded."
+Write-Host "Downloading puppet installer."
+$WebClient.DownloadFile($PuppetURL, $PuppetDestination)
+Write-Host "Puppet installer successfully downloaded."
 
 Set-ExecutionPolicy bypass
 $env:SEE_MASK_NOZONECHECKS=1
@@ -48,8 +48,9 @@ $WixInstallArgs = @("/quiet", "/norestart")
 Write-Host "Installing Wix toolset."
 $WixProcess = Start-Process -FilePath $WixDestination -ArgumentList $WixInstallArgs -Wait -PassThru
 
-Write-Host "Starting puppet installation script."
-Invoke-Expression $PuppetInstallDestination
+Write-Host "Installing Puppet."
+$PuppetInstallArgs = @("/qn", "/norestart", "/i", $PuppetDestination)
+$PuppetProcess = Start-Process -FilePath msiexec.exe -ArgumentList $PuppetInstallArgs -Wait -PassThru
 
 Write-Host "Starting substrate build"
 mkdir C:\vagrant\substrate-assets
