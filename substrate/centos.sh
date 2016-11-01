9188069297a578afa59cecf3c7462997b0a31c95
Chris Roberts <code@chrisroberts.org>
2016-11-01 10:03:10 -0700

    move location

---
+++ substrate/centos.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# if the proxy is around, use it
+nc -z -w3 192.168.1.1 8123 && export http_proxy="http://192.168.1.1:8123"
+
+REPO_RPM_URL="http://yum.puppetlabs.com/puppetlabs-release-el-5.noarch.rpm"
+rm -f /tmp/puppet.rpm
+curl -o /tmp/puppet.rpm -L $REPO_RPM_URL
+rpm -i /tmp/puppet.rpm
+yum install -y puppet
+
+mkdir -p /vagrant/substrate-assets
+/vagrant/substrate/run.sh /vagrant/substrate-assets
--- substrate/ubuntu.sh
+++ substrate/ubuntu.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# if the proxy is around, use it
+nc -z -w3 192.168.1.1 8123 && export http_proxy="http://192.168.1.1:8123"
+
 DEBIAN_FRONTEND=noninteractive apt-get update -yq
 DEBIAN_FRONTEND=noninteractive apt-get install -yq ruby libopenssl-ruby rubygems1.8
 gem install json_pure -v '~> 1.0' --no-ri --no-rdoc
