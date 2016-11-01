49badb42979fd87c96b334996636238788123a2e
Chris Roberts <code@chrisroberts.org>
2016-11-01 06:09:18 -0700

    powershell tweaks

--- win.ps1
+++ win.ps1
@@ -6,6 +6,23 @@
     Installs required software and builds substrate
 #>
 
+# http://www.leeholmes.com/blog/2008/07/30/workaround-the-os-handles-position-is-not-what-filestream-expected/
+# http://stackoverflow.com/questions/8978052/powershell-2-0-redirection-file-handle-exception
+function savepowershellfromitself {
+    $bindingFlags = [Reflection.BindingFlags] "Instance,NonPublic,GetField"
+    $objectRef = $host.GetType().GetField( "externalHostRef", $bindingFlags ).GetValue( $host )
+    $bindingFlags = [Reflection.BindingFlags] "Instance,NonPublic,GetProperty"
+    $consoleHost = $objectRef.GetType().GetProperty( "Value", $bindingFlags ).GetValue( $objectRef, @() )
+    [void] $consoleHost.GetType().GetProperty( "IsStandardOutputRedirected", $bindingFlags ).GetValue( $consoleHost, @() )
+    $bindingFlags = [Reflection.BindingFlags] "Instance,NonPublic,GetField"
+    $field = $consoleHost.GetType().GetField( "standardOutputWriter", $bindingFlags )
+    $field.SetValue( $consoleHost, [Console]::Out )
+    $field2 = $consoleHost.GetType().GetField( "standardErrorWriter", $bindingFlags )
+    $field2.SetValue( $consoleHost, [Console]::Out )
+}
+
+savepowershellfromitself
+
 $DotNetUpgradeURL = "https://download.microsoft.com/download/E/2/1/E21644B5-2DF2-47C2-91BD-63C560427900/NDP452-KB2901907-x86-x64-AllOS-ENU.exe"
 $BuildToolsURL = "https://download.microsoft.com/download/E/E/D/EEDF18A8-4AED-4CE0-BEBE-70A83094FC5A/BuildTools_Full.exe"
 $WixURL = "http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=wix&DownloadId=1587179&FileTime=131118854865270000&Build=21031"
@@ -54,4 +71,7 @@ $PuppetProcess = Start-Process -FilePath msiexec.exe -ArgumentList $PuppetInstal
 
 Write-Host "Starting substrate build"
 mkdir C:\vagrant\substrate-assets
+
+savepowershellfromitself
+
 Invoke-Expression "C:\vagrant\substrate\run.ps1 C:\vagrant\substrate-assets"
